5a645ab7958de651ff3319bb517d67397046fbb1
omarkhaled11 <omarkhaled320@hotmail.com>
2019-04-15 16:00:03 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,36 @@ import React, { Component } from 'react'
 
 const style = {
   container: {
-    flex: 1,
     height: '100%',
     width: '100%',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'space-around',
     alignItems: 'center',
+    textAlign: 'center',
   },
   count: {
-    fontSize: 120,
+    textAlign: 'center',
+    fontSize: 80,
     flex: 1,
     marginTop: 20,
+    marginBottom: 20,
+  },
+  text: {
+    fontSize: 30,
+    flex: 1,
+    marginTop: 20,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  arrows: {
+    flexDirection: 'row',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: 200,
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     marginBottom: 20,
   },
   button: {
@@ -21,6 +39,9 @@ const style = {
     width: 200,
     fontSize: 30,
     flex: 1,
+    textAlign: 'center',
+    backgroundColor: 'red',
+    color: 'white',
   }
 }
 
@@ -42,7 +63,16 @@ class App extends Component {
       <div style={style.container}>
         <div style={style.count}>XM NAGY</div>
         <div style={style.count}>{count}</div>
-        <button style={style.button} onClick={this.handleClick}>
+        <div style={style.text}>If you want to know how many times xm nagy click here</div>
+        <div style={style.arrows}>
+          <img width={30} height={30} src="https://discover.engineering.utoronto.ca/files/2014/06/arrow-down.jpg" />
+          <img width={30} height={30} src="https://discover.engineering.utoronto.ca/files/2014/06/arrow-down.jpg" />
+          <img width={30} height={30} src="https://discover.engineering.utoronto.ca/files/2014/06/arrow-down.jpg" />
+          <img width={30} height={30} src="https://discover.engineering.utoronto.ca/files/2014/06/arrow-down.jpg" />
+          <img width={30} height={30} src="https://discover.engineering.utoronto.ca/files/2014/06/arrow-down.jpg" />
+          <img width={30} height={30} src="https://discover.engineering.utoronto.ca/files/2014/06/arrow-down.jpg" />
+        </div>
+        <button autoFocus style={style.button} onClick={this.handleClick}>
           XM NAGY
         </button>
       </div>
